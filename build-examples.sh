4dbff27c597bd5d04b98a30afffb945cd751fb05
Ryan Rempel <rgrempel@gmail.com>
2018-05-17 22:34:41 -0500

    Another example.

--- build-examples.sh
+++ build-examples.sh
@@ -5,3 +5,4 @@ pulp browserify -m Examples.UnorderedList -t build/UnorderedList.js -I examples 
 pulp browserify -m Examples.Buttons -t build/Buttons.js -I examples -j 4 --optimise
 pulp browserify -m Examples.Field -t build/Field.js -I examples -j 4 --optimise
 pulp browserify -m Examples.Form -t build/Form.js -I examples -j 4 --optimise
+pulp browserify -m Examples.Random -t build/Random.js -I examples -j 4 --optimise
---
+++ examples/Random.purs
@@ -0,0 +1,86 @@
+module Examples.Random where
+
+import Elm.Default
+
+import Control.Monad.Aff (launchAff_)
+import Control.Monad.Eff (Eff)
+import Control.Monad.IO (INFINITY, runIO)
+import Elm.Platform (runProgram)
+import Elm.Platform.Cmd (none) as Cmd
+import Elm.Platform.Sub (none) as Sub
+import Elm.Random (generate, int) as Random
+import Html (Html, button, div, h1, program, text)
+import Html.Events (onClick)
+import Prelude (class Eq, Unit, unit, ($))
+
+
+
+main :: Eff (infinity :: INFINITY) Unit
+main =
+    launchAff_ $ runIO do
+        runProgram unit $
+            program
+                { init
+                , view
+                , update
+                , subscriptions
+                }
+
+
+-- MODEL
+
+
+type Model =
+    { dieFace :: Int
+    }
+
+
+mkModel :: Int -> Model
+mkModel = { dieFace : _ }
+
+
+init :: (Model /\ Cmd Msg)
+init =
+    ( mkModel 1 /\ Cmd.none )
+
+
+
+-- UPDATE
+
+
+data Msg
+  = Roll
+  | NewFace Int
+
+derive instance eqMsg :: Eq Msg
+
+
+update :: Msg -> Model -> (Model /\ Cmd Msg)
+update msg model =
+  case msg of
+    Roll ->
+      (model /\ Random.generate NewFace (Random.int 1 6))
+
+    NewFace newFace ->
+      (mkModel newFace /\ Cmd.none)
+
+
+
+-- SUBSCRIPTIONS
+
+
+subscriptions :: Model -> Sub Msg
+subscriptions model =
+  Sub.none
+
+
+
+-- VIEW
+
+
+view :: Model -> Html Msg
+view model =
+  div []
+    [ h1 [] [ text (toString model.dieFace) ]
+    , button [ onClick Roll ] [ text "Roll" ]
+    ]
